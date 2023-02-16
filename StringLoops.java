ce88c96a40468d711b89fc9d2ac318b1fd343f4d
tammy vu <tammy.vsk@gmail.com>
2023-02-16 11:36:49 -0600

    does something

--- StringLoops.java
+++ StringLoops.java
@@ -2,15 +2,17 @@ public class StringLoops {
 
   public static String reverseCharacters(String a) {
     String newWord = "";
-    for (int i = a.length(); i > 0; i--){
-      newWord = a.substring(i-1, i);
+    for (int i = a.length()-1; i >= 0; i--){
+      String currentLetter = a.substring(a.length()-1, a.length());
+      newWord = newWord + currentLetter; 
+      System.out.println(newWord);
     }
     return newWord;
   }
 
   public static void main(String[] args) {
 
-    reverseCharacters("akld");
+    reverseCharacters("akldfhcxxkjyt");
 
   }
 }
