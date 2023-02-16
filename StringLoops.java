b243b08e33a3e4efbb4af254f93644c6ea3c992a
tammy vu <tammy.vsk@gmail.com>
2023-02-16 11:58:59 -0600

    fin

--- StringLoops.java
+++ StringLoops.java
@@ -2,17 +2,20 @@ public class StringLoops {
 
   public static String reverseCharacters(String a) {
     String newWord = "";
-    for (int i = a.length()-1; i >= 0; i--){
-      String currentLetter = a.substring(a.length()-1, a.length());
+    for (int x = 1; x < a.length(); x++){
+      String currentLetter = a.substring(a.length()-x, a.length()-(x-1));
       newWord = newWord + currentLetter; 
       System.out.println(newWord);
     }
+    String lastletter = a.substring(0,1);  
+    newWord = newWord + lastletter;
+    System.out.println(newWord);
     return newWord;
   }
 
   public static void main(String[] args) {
 
-    reverseCharacters("akldfhcxxkjyt");
+    reverseCharacters("dalmation");
 
   }
 }
