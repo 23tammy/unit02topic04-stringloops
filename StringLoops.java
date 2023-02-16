8559e9ee7df85bbf117e2586fd34aab52be815a7
tammy vu <tammy.vsk@gmail.com>
2023-02-16 12:21:45 -0600

    testing

--- StringLoops.java
+++ StringLoops.java
@@ -2,20 +2,25 @@ public class StringLoops {
 
   public static String reverseCharacters(String a) {
     String newWord = "";
-    for (int x = 1; x < a.length(); x++){
+    for (int x = 1; x <= a.length(); x++){
       String currentLetter = a.substring(a.length()-x, a.length()-(x-1));
       newWord = newWord + currentLetter; 
-      System.out.println(newWord);
     }
-    String lastletter = a.substring(0,1);  
-    newWord = newWord + lastletter;
-    System.out.println(newWord);
     return newWord;
+  }
+  public static void indexesOfAll(String text, String searchTerm) {
+  int[] askjhdfakjsfh;
+    for (int i = 0; i < text.length(); i++){
+      if (text.substring(i, i+1).equals(searchTerm)){
+        System.out.println(i);
+        
+      }
+    } 
+  
   }
 
   public static void main(String[] args) {
-
-    reverseCharacters("dalmation");
-
+    System.out.println(reverseCharacters("sans"));
+   indexesOfAll("marry poppins", "p");
   }
 }
